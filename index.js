59c20f7b650da0331dc797f33f715fc2ef7fe84a
jjiarongcao1 <alexjcao@outlook.com>
2020-05-26 22:13:30 -0400

    finished task 3.

--- index.js
+++ index.js
@@ -3,42 +3,81 @@
 // 🏡 Task 1: Variables
 /* Create variables for principal, interest rate, and years. Assign them the values 200000, 0.05, and 30 respectively. Create another value called name and give it the value of your own name.
 */
+// let p = 200000;
+// let r = 0.05;
+// let years = 30;
+// const name = 'Alex Cao';
 
 
 
 
+// // 🏡 Task 1.5: Simple Math
+// /* To create a monthly mortgage rate calculator, we need to know the number of years in months and the monthly interest rate. 
 
-// 🏡 Task 1.5: Simple Math
-/* To create a monthly mortgage rate calculator, we need to know the number of years in months and the monthly interest rate. 
+// Create a variable called `monthlyInterestRate` and give it the value of interest rate divided by 12. 
 
-Create a variable called `monthlyInterestRate` and give it the value of interest rate divided by 12. 
-
-Create another variable called `periods` and give it the value of years*12.
-*/
-
-
-
-
-// 🏡 Task 2: Harder Math
-/* Create your calculator! Use the formula in the ReadMe to run calculations on your numbers. Save the final value into a variable called monthlyRate.
-
-Hint: while these calculations can be done in one line, it might be helpful to create a variable called "numerator" to calculate the numerator, and another called "denominator" to calculate the denominator 
-
-Hint #2: you'll need to use the `math` object for parts of this calculation!
-
-When your math is correct, monthlyRate will equal 1073.64
-*/
+// Create another variable called `periods` and give it the value of years*12.
+// */
+// let monthlyInterestRate = r / 12;
+// let periods = years * 12;
 
 
 
+// // 🏡 Task 2: Harder Math
+// /* Create your calculator! Use the formula in the ReadMe to run calculations on your numbers. Save the final value into a variable called monthlyRate.
+
+// Hint: while these calculations can be done in one line, it might be helpful to create a variable called "numerator" to calculate the numerator, and another called "denominator" to calculate the denominator 
+
+// Hint #2: you'll need to use the `math` object for parts of this calculation!
+
+// When your math is correct, monthlyRate will equal 1073.64
+// */
+// let numerator1 = 1 + monthlyInterestRate;
+// let numerator2 = Math.pow(numerator1, periods);
+// let numerator3 = monthlyInterestRate * numerator2;
+
+// let denominator1 = 1 + monthlyInterestRate;
+// let denominator2 = Math.pow(denominator1, periods) - 1
+
+// let monthlyRate = p * (numerator3 / denominator2);
+
+// let fixMonthlyRate = monthlyRate.toFixed(2);
+
+// console.log(numerator1);
+// console.log(numerator2);
+// console.log(numerator3);
+// console.log(denominator1);
+// console.log(denominator2);
+// console.log(fixMonthlyRate);
 
 // 🏡 Task 3: Function
 /* Create a function called `mortgageCalculator` that combines all of the steps from task 1 and 2 and returns a sentence "{Name}, your monthly rate is ${monthlyRate}"
 
 If your name is `Oscar` mortgageCalculator() should return "Oscar, your monthly rate is 1073.64"
 */
+function mortgageCalculator() {
+    let p = 200000;
+    let r = 0.05;
+    let years = 30;
+    const name = 'Alex Cao';
+    let monthlyInterestRate = r / 12;
+    let periods = years * 12;
+    let numerator1 = 1 + monthlyInterestRate;
+    let numerator2 = Math.pow(numerator1, periods);
+    let numerator3 = monthlyInterestRate * numerator2;
 
+    let denominator1 = 1 + monthlyInterestRate;
+    let denominator2 = Math.pow(denominator1, periods) - 1
 
+    let monthlyRate = p * (numerator3 / denominator2);
+
+    let fixMonthlyRate = monthlyRate.toFixed(2);
+
+    console.log(name + ", " + "your month rate is " + fixMonthlyRate);
+
+}
+
+mortgageCalculator();
 
 
 
